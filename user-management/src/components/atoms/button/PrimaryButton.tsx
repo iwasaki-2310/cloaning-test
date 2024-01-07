7c27da967d1881236776f0a0ebf33f507c37db5e
Iwasaki-2310 <iwachan2310@gmail.com>
2024-01-07 17:19:49 +0900

    ログイン機能実装

--- user-management/src/components/atoms/button/PrimaryButton.tsx
+++ user-management/src/components/atoms/button/PrimaryButton.tsx
@@ -4,12 +4,22 @@ import { Button } from '@chakra-ui/react'
 
 type Props = {
   children: ReactNode
+  disabled?: boolean
+  loading?: boolean
+  onClick: () => void
 }
 
 export const PrimaryButton: FC<Props> = memo((props) => {
-  const { children } = props
+  const { children, disabled = false, loading = false, onClick } = props
   return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.6 }}>
+    <Button
+      bg="teal.400"
+      color="white"
+      _hover={{ opacity: 0.6 }}
+      isDisabled={disabled || loading}
+      isLoading={loading}
+      onClick={onClick}
+    >
       {children}
     </Button>
   )
