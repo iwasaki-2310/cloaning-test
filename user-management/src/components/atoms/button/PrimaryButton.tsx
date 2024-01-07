dfcde6c76fcc2b2ed8dd26de0714220a06842805
Iwasaki-2310 <iwachan2310@gmail.com>
2024-01-07 15:21:11 +0900

    ログインページ_ログインボタンをコンポーネント化

---
+++ user-management/src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode, memo } from 'react'
+
+import { Button } from '@chakra-ui/react'
+
+type Props = {
+  children: ReactNode
+}
+
+export const PrimaryButton: FC<Props> = memo((props) => {
+  const { children } = props
+  return (
+    <Button bg="teal.400" color="white" _hover={{ opacity: 0.6 }}>
+      {children}
+    </Button>
+  )
+})
