bbb685456d4e32cd565a394d3ef27ec4a10f990e
Iwasaki-2310 <iwachan2310@gmail.com>
2024-01-08 00:18:52 +0900

    一覧画面作成中

--- user-management/src/components/pages/UserManagement.tsx
+++ user-management/src/components/pages/UserManagement.tsx
@@ -1,5 +1,13 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from '@chakra-ui/react'
 import { FC, memo } from 'react'
+import { UserCard } from '../organisms/user/UserCard'
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー管理ページ</p>
+  return (
+    <Wrap p={{base: 4, md:10}}>
+      <WrapItem>
+        <UserCard imageUrl='https://www.livefreely2310.com/my-family/wp-content/uploads/2023/04/%E7%B5%90%E5%A9%9A%E5%BC%8F.jpg' userName='静' fullName='Shizuka Iwasaki' />
+      </WrapItem>
+    </Wrap>
+  )
 })
