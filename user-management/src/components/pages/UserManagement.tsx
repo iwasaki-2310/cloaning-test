1986321e6a42beed26a1644a3814d555feb0d1c2
Iwasaki-2310 <iwachan2310@gmail.com>
2024-01-08 00:55:30 +0900

    一覧画面作成

--- user-management/src/components/pages/UserManagement.tsx
+++ user-management/src/components/pages/UserManagement.tsx
@@ -1,13 +1,30 @@
-import { Box, Image, Stack, Text, Wrap, WrapItem } from '@chakra-ui/react'
-import { FC, memo } from 'react'
+import { Box, Center, Image, Spinner, Stack, Text, Wrap, WrapItem } from '@chakra-ui/react'
+import { FC, memo, useEffect } from 'react'
 import { UserCard } from '../organisms/user/UserCard'
+import { useAllUsers } from '../../hooks/useAllUsers'
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers()
+  useEffect(() => getUsers(), [])
   return (
-    <Wrap p={{base: 4, md:10}}>
-      <WrapItem>
-        <UserCard imageUrl='https://www.livefreely2310.com/my-family/wp-content/uploads/2023/04/%E7%B5%90%E5%A9%9A%E5%BC%8F.jpg' userName='静' fullName='Shizuka Iwasaki' />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center height="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} m="auto">
+              <UserCard
+                imageUrl="https://www.livefreely2310.com/my-family/wp-content/uploads/2023/04/%E7%B5%90%E5%A9%9A%E5%BC%8F.jpg"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   )
 })
