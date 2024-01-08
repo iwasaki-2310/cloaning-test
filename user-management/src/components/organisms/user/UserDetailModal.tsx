25dba1db53a5496129ab79a0572463766e7573b2
Iwasaki-2310 <iwachan2310@gmail.com>
2024-01-08 17:08:06 +0900

    モーダル機能実装

--- user-management/src/components/organisms/user/UserDetailModal.tsx
+++ user-management/src/components/organisms/user/UserDetailModal.tsx
@@ -1,8 +1,5 @@
 import {
-  Box,
-  Image,
   Stack,
-  Text,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -14,14 +11,16 @@ import {
   Input,
 } from '@chakra-ui/react'
 import { FC, memo } from 'react'
+import { User } from '../../../types/api/user'
 
 type Props = {
+  user: User | null
   isOpen: boolean
   onClose: () => void
 }
 
 export const UserDetailModal: FC<Props> = memo((props) => {
-  const { isOpen, onClose } = props
+  const {user, isOpen, onClose } = props
   return (
     <Modal isOpen={isOpen} onClose={onClose} autoFocus={false} motionPreset="slideInBottom">
       <ModalOverlay>
@@ -32,19 +31,19 @@ export const UserDetailModal: FC<Props> = memo((props) => {
             <Stack spacing={4}>
               <FormControl>
                 <FormLabel>名前</FormLabel>
-                <Input value="しずか" isReadOnly />
+                <Input value={user?.username} isReadOnly />
               </FormControl>
               <FormControl>
                 <FormLabel>フルネーム</FormLabel>
-                <Input value="Shizuka Iwasaki" isReadOnly />
+                <Input value={user?.name} isReadOnly />
               </FormControl>
               <FormControl>
                 <FormLabel>MAIL</FormLabel>
-                <Input value="12345@example.com" isReadOnly />
+                <Input value={user?.email} isReadOnly />
               </FormControl>
               <FormControl>
                 <FormLabel>TEL</FormLabel>
-                <Input value="090-0000-000" isReadOnly />
+                <Input value={user?.phone} isReadOnly />
               </FormControl>
             </Stack>
           </ModalBody>
--- user-management/src/components/pages/UserManagement.tsx
+++ user-management/src/components/pages/UserManagement.tsx
@@ -3,13 +3,21 @@ import { FC, memo, useCallback, useEffect } from 'react'
 import { UserCard } from '../organisms/user/UserCard'
 import { useAllUsers } from '../../hooks/useAllUsers'
 import { UserDetailModal } from '../organisms/user/UserDetailModal'
+import { useSelectUser } from '../../hooks/useSelectUser'
 
 export const UserManagement: FC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { getUsers, loading, users } = useAllUsers()
+  const { onSelectUser, selectedUser } = useSelectUser()
+
   useEffect(() => getUsers(), [])
 
-  const onClickUser = useCallback(() => onOpen(), [])
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen })
+    },
+    [users, onSelectUser, onOpen]
+  )
   return (
     <>
       {loading ? (
@@ -21,6 +29,7 @@ export const UserManagement: FC = memo(() => {
           {users.map((user) => (
             <WrapItem key={user.id} m="auto">
               <UserCard
+                id={user.id}
                 imageUrl="https://www.livefreely2310.com/my-family/wp-content/uploads/2023/04/%E7%B5%90%E5%A9%9A%E5%BC%8F.jpg"
                 userName={user.username}
                 fullName={user.name}
@@ -30,7 +39,7 @@ export const UserManagement: FC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
     </>
   )
 })
